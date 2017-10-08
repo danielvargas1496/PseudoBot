5f348b50573539ff8ac69bae76f956f5d6bac63b
danielvargas1496 <32419227+danielvargas1496@users.noreply.github.com>
2017-10-08 05:55:19 -0500

    Add files via upload

---
+++ configTemplate.js
@@ -0,0 +1,10 @@
+
+//find keys and access tokens at:
+//https://apps.twitter.com
+
+module.exports = {
+    consumer_key: '...',
+    consumer_secret: '...',
+    access_token: '...',
+    access_token_secret: '...',
+}
